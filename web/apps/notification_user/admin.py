760b16029cae4da4cc6ac5938d9526f17425f6a0
Pavel Pantyukhov <pantyukhov.p@gmail.com>
2018-10-11 12:24:26 +0300

    added models to admin panel

--- web/apps/notification_user/admin.py
+++ web/apps/notification_user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from apps.notification_user.models import User, UserConnectionField
+
+admin.site.register(User)
+admin.site.register(UserConnectionField)
+
--- web/apps/notification_user/models.py
+++ web/apps/notification_user/models.py
@@ -5,8 +5,25 @@ from django.db import models
 
 
 
+
+
 class User(models.Model):
     reffer_id = models.CharField('Id', max_length=1000)
 
     def __str__(self):
         return self.id
+
+
+class UserConnectionField(models.Model):
+    EMAIL_TYPE = 1
+    GCS_TYPE = 2
+    TYPE_CHOICES = (
+        (EMAIL_TYPE, 'Email'),
+        (GCS_TYPE, 'Google cloud message'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=True)
+
+    value = models.CharField('value', max_length=1000)
+
+    type = models.IntegerField(choices=TYPE_CHOICES, default=EMAIL_TYPE)
