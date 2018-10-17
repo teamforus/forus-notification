be18c6ffa0ffd8c8d5525734d2aecdc9c8cc3930
Pavel Pantyukhov <pantyukhov.p@gmail.com>
2018-10-17 16:38:37 +0300

    fix get user

--- web/apps/sender/tasks.py
+++ web/apps/sender/tasks.py
@@ -7,7 +7,7 @@ from apps.notification_user.models import User
 
 
 @app.task(bind=True)
-def send_email(task, user_pk, template, data):
-    user = User.objects.filter(pk=user_pk).get()
+def send_email(task, reffer_user_id, template, data):
+    user = User.objects.filter(reffer_id=reffer_user_id).get()
     email_sender = EmailSender(user)
     email_sender.send_email(template, data)
