9f6553de507fa2cbd2555b8ffa330c441d6ef2e5
Pavel Pantyukhov <pantyukhov.p@gmail.com>
2018-10-18 11:05:01 +0300

    set EMAIL_USE_SSL to config

--- web/app/settings.py
+++ web/app/settings.py
@@ -125,7 +125,8 @@ EMAIL_HOST = os.environ.get('EMAIL_HOST', '')
 EMAIL_PORT = os.environ.get('EMAIL_PORT', '')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', True)
+EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SLL', False)
 
 
 # i18
