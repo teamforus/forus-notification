679d63aefe5d78fec73848223d23cf41882f50fe
Pavel Pantyukhov <pantyukhov.p@gmail.com>
2018-10-30 11:02:26 +0300

    added import export

--- web/apps/notification_user/admin.py
+++ web/apps/notification_user/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
 
-
 # Register your models here.
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
 from apps.notification_user.models import User, UserConnectionField
 
-admin.site.register(User)
-admin.site.register(UserConnectionField)
 
+class UserConnectionFieldResource(resources.ModelResource):
+    class Meta:
+        model = UserConnectionField
+
+
+class UserResource(resources.ModelResource):
+    class Meta:
+        model = User
+
+
+class UserAdmin(ImportExportModelAdmin):
+    resource_class = UserResource
+
+
+class UserConnectionFieldAdmin(ImportExportModelAdmin):
+    resource_class = UserConnectionFieldResource
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(UserConnectionField, UserConnectionFieldAdmin)
