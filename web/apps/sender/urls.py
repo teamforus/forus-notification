c675a3584e0ee8c283e877c7de50537da0f1e7c8
Pavel Pantyukhov <pantyukhov.p@gmail.com>
2018-10-31 12:56:58 +0300

    change slesh

--- web/apps/sender/urls.py
+++ web/apps/sender/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
 
     url(r'^mobile/push', views.PushNotificationView.as_view(), name='mobile-send-push'),
     # url(r'^products$', views.ProductList.as_view(), name='product.py-list'),
-    url(r'^vouchers/new_fund$', views.NewFund.as_view(), name='new-fund-email'),
-    url(r'^vouchers/new_fund_created$', views.NewFundCreated.as_view(), name='new-fund-created-email'),
+    url(r'^vouchers/new_fund', views.NewFund.as_view(), name='new-fund-email'),
+    url(r'^vouchers/new_fund_created', views.NewFundCreated.as_view(), name='new-fund-created-email'),
     url(r'^vouchers/provider_approved', views.ProviderApprovedView.as_view(), name='provider-approved-email'),
     url(r'^vouchers/provider_applied', views.ProviderAppliedView.as_view(), name='provider-applied-email'),
     url(r'^vouchers/new_product_added', views.NewProductAddedView.as_view(), name='new-product-added-email'),
