36c6e5e3e531ca45395b879ee2c9ddd5af75006c
Pavel Pantyukhov <pantyukhov.p@gmail.com>
2018-11-16 15:42:08 +0300

    fix header bug

--- web/apps/sender/tasks.py
+++ web/apps/sender/tasks.py
@@ -7,13 +7,13 @@ from apps.notification_user.models import User
 
 
 #@app.task(bind=True)
-def send_mobile_push(task, reffer_user_id, title, body):
+def send_mobile_push(self, reffer_user_id, title, body):
     user = User.objects.filter(reffer_id=reffer_user_id).get()
     email_sender = FcmSender(user)
     email_sender.send_push(title, body)
 
 @app.task(bind=True)
-def send_email(task, reffer_user_id, template, data):
+def send_email(self, reffer_user_id, template, data):
     user = User.objects.filter(reffer_id=reffer_user_id).get()
     email_sender = EmailSender(user)
     email_sender.send_email(template, data)
