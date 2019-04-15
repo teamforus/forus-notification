c6ff68bd22685c0eec9481e5e3f14c380f38c758
Павел Пантюхов <pantyukhov.p@gmail.com>
2019-04-15 17:50:14 +0300

    fix payload

--- web/apps/email_sender/apns_sender.py
+++ web/apps/email_sender/apns_sender.py
@@ -1,7 +1,7 @@
 import os
 
 from apns2.client import APNsClient
-from apns2.payload import Payload
+from apns2.payload import Payload, PayloadAlert
 from django.conf import settings
 from fcm_django.models import FCMDevice
 from mail_templated import send_mail
@@ -18,6 +18,6 @@ class Sender():
 
         client = APNsClient(settings.APNS_KEY_LOCATION, use_sandbox=settings.APNS_SENDBOX, use_alternative_port=False)
 
-        payload = Payload(alert=title, sound="default", badge=1)
+        payload = Payload(alert=PayloadAlert(title=title, body=body), sound="default", badge=1)
         for connect in connects:
             client.send_notification(token_hex=connect.value, notification=payload, topic=settings.APNS_BUNDLE_ID)
